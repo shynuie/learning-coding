dc7fa3c37808644e9df83944f29a670fb2f877b4
shynuie <Marvin@fanyuxiande-MBP.lan>
2018-11-17 16:29:30 +0800

    优化猜数字小游戏

--- 猜数字小游戏.py
+++ 猜数字小游戏.py
@@ -2,7 +2,7 @@
 import random;
 import time;
 print('\n来来来，这是一个超适合团康活动用的超无聊小游戏，叫做猜数字游戏\n规则很简单，请先输入您的游玩人数-');
-people_qty = input('\n人数：');
+people_qty = input('\n人数(阿拉伯数字)：');
 people_qty = int(people_qty);
 numb_list = list(range(1, people_qty+1));
 random.shuffle(numb_list);
@@ -23,8 +23,7 @@ while(max_numb < 3):
 min_numb = 0;
 goal_numb = random.randint(1,max_numb);
 j = 0;
-type_numb = 0;
-while(type_numb != goal_numb):
+while(True):
 	print('\n请顺序第"', j%people_qty+1, '"位玩家键入猜测的数字(目前最小值为:', min_numb, '，最大值为：', max_numb,'。)' );
 	type_numb = input('请输入阿拉伯数字后按Enter继续：');
 	type_numb = int(type_numb);
@@ -42,10 +41,9 @@ while(type_numb != goal_numb):
 	print('.');
 	if(type_numb < goal_numb):
 		min_numb = type_numb;
-		print('没中，', min_numb, '到', max_numb, '之间。');
 	elif(type_numb > goal_numb):
 		max_numb = type_numb;
-		print('没中，', min_numb, '到', max_numb, '之间。');
 	else:
 		print('哈哈哈哈哈哈哈！！！！！\n你这个倒霉鬼！！!！！\n这么倒霉不弄你弄谁！！！！！');
 		break;
+	print('没中，', min_numb, '到', max_numb, '之间。');
