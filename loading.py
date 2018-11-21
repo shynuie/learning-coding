aa0ebc70c2de65dde2d60f698f67bbcef999f4aa
shynuie <Marvin@fanyuxiande-MacBook-Pro.local>
2018-11-21 18:16:59 +0800

    优化：使用快写法

--- loading.py
+++ loading.py
@@ -13,19 +13,13 @@ with open('reviews.txt','r') as f:
 
     dis_numb = input('显示字元数量以上资料：');
     dis_numb = int(dis_numb);
-    comment_1 = [];
-    for line in comment:
-        if len(line) > dis_numb:
-            comment_1.append(line);
+    comment_1 = [line for line in comment if len(line) > dis_numb];
     print('共', len(comment_1), '笔资料字元超过', dis_numb, '字元。');
     view_numb = input('要显示第几笔筛选的资料？\n');
     view_numb = int(view_numb);
     print(comment_1[view_numb]);
     search_word = input('请输入搜索的关键字：');
-    searched_comment = [];
-    for line in comment:
-        if search_word in line:
-            searched_comment.append(line);
+    searched_comment = [line for line in comment if search_word in line];
     print('搜索到', len(searched_comment), '笔资料。');
     view_numb = input('要显示第几笔筛选的资料？\n');
     view_numb = int(view_numb);
