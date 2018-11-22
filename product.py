2300300711227257602ba9a1367ae54ae4da3ee7
Shynuie <yuhsien7831@icloud.com>
2018-11-22 16:50:18 +0800

    优化功能新增写入档案功能

--- product.py
+++ product.py
@@ -1,9 +1,13 @@
-product = [];
+products = [];
 while True:
     name = input('請輸入商品名稱(中止請直接按Enter)：');
     if name == '':
         break;
     price = input('請設定' + name + '的價格:');
     price = int(price);
-    product.append([name, price]);
-print(product);
+    products.append([name, price]);
+for p in products:
+    print(p);
+with open ('product_info.csv', 'w', encoding = 'utf-8') as f:
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n');
