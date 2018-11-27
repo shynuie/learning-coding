43b307ff18905d0c2df9bccaf56277d4c7924bfe
Shynuie <yuhsien7831@icloud.com>
2018-11-27 22:46:42 +0800

    更新读取与存档功能

--- product.py
+++ product.py
@@ -1,4 +1,13 @@
+import os
 products = [];
+if os.path.isfile('product_info.csv'):
+    with open ('product_info.csv', 'r') as f:
+        for p in f:
+            if '商品,價格' in p:
+                continue;
+            name, price = p.strip().split(',');
+            products.append([name, price]);
+    print('读档成功，現有商品資料：\n', products);
 while True:
     name = input('請輸入商品名稱(中止請直接按Enter)：');
     if name == '':
@@ -6,8 +15,6 @@ while True:
     price = input('請設定' + name + '的價格:');
     price = int(price);
     products.append([name, price]);
-for p in products:
-    print(p);
 with open ('product_info.csv', 'w', encoding = 'utf-8') as f:
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n');
